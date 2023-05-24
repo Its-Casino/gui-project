7a4e1d53a15b00a7c52a6c3d1333bea3d13074b3
rilind404 <117649966+rilind404@users.noreply.github.com>
2023-05-24 16:23:59 +0200

    Rebuilt checkout

---
+++ src/imat/CheckoutPage.java
@@ -0,0 +1,119 @@
+package imat;
+
+import java.io.IOException;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+import se.chalmers.cse.dat216.project.IMatDataHandler;
+
+
+
+public class CheckoutPage extends AnchorPane {
+
+
+    private MainViewController parentController;
+    @FXML
+    private ImageView varukorg_wizard_state_image;
+    @FXML
+    private ImageView leveransadress_wizard_state_image;
+    @FXML
+    private ImageView leveranstid_wizard_state_image;
+    @FXML
+    private ImageView betalning_wizard_state_image;
+
+
+    @FXML
+    private Label din_varukorg_antal_varor;
+    @FXML
+    private Label din_varukorg_totalt_kostnad;
+
+
+    @FXML
+    private TextField leveransadress_fornamn;
+    @FXML
+    private TextField leveransadress_efternamn;
+    @FXML
+    private TextField leveransadress_gatuadress;
+    @FXML
+    private TextField leveransadress_postnummer;
+    @FXML
+    private TextField leveransadress_postort;
+    @FXML
+    private TextField leveransadress_mobilnummer;
+    @FXML
+    private TextField leveransadress_hemtelefon;
+    @FXML
+    private CheckBox spara_adress_checkbox;
+    @FXML
+    private CheckBox stall_kassorna_checkbox;
+
+
+    @FXML
+    private ComboBox leveranstid_dag;
+    @FXML
+    private ComboBox leveranstid_manad;
+    @FXML
+    private Label datum_tillgangligt_state;
+    @FXML
+    private ImageView datum_tillgangligt_state_image;
+    @FXML
+    private RadioButton leveranstid_07_11_button;
+    @FXML
+    private RadioButton leveranstid_08_12_button;
+    @FXML
+    private RadioButton leveranstid_09_13_button;
+    @FXML
+    private RadioButton leveranstid_10_14_button;
+    @FXML
+    private RadioButton leveranstid_11_15_button;
+    @FXML
+    private Label leveranstid_vald_datum;
+    @FXML
+    private Label leveranstid_vald_tid;
+
+
+    @FXML
+    private TextField betalning_kortnummer;
+    @FXML
+    private TextField betalning_manad_ar;
+    @FXML
+    private TextField betalning_cvc;
+    @FXML
+    private CheckBox betalning_spara_betalning;
+    @FXML
+    private Label betalning_antal_varor;
+    @FXML
+    private Label betalning_moms;
+    @FXML
+    private Label betalning_total_kostnad;
+
+    private IMatDataHandler datahandler = IMatDataHandler.getInstance();
+
+    public CheckoutPage(MainViewController parentController) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("utcheckning.fxml"));
+        fxmlLoader.setController(this);
+
+
+
+
+
+
+
+
+
+
+
+
+
+        try {
+            fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        this.parentController = parentController;
+    }
+
+}
